a81744e6ade7c621e8d48e17a21fcf3d848e9a87
João <arantesjoaov@gmail.com>
2023-06-07 14:31:51 -0300

    finished register ui, created the prisma models and implemented the register funcionality

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -14,6 +14,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
 import Heading from '../Heading'
 import Input from '../inputs/Input'
+import { toast } from 'react-hot-toast'
+import Button from '../Button'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
@@ -34,7 +36,7 @@ const RegisterModal = () => {
     axios.post('/api/register', data).then(() => {
       registerModal.onClose()
     }).catch((error) => {
-      console.log(error)
+      toast.error('Something went wrong!')
     }).finally(() => {
       setIsLoading(false)
     })
@@ -54,6 +56,54 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
+      <Input
+        id='name'
+        label='Name'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='password'
+        type='password'
+        label='Password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label='Continue with Google'
+        icon={FcGoogle}
+        onClick={() => { }}
+      />
+      <Button
+        outline
+        label='Continue with Github'
+        icon={AiFillGithub}
+        onClick={() => { }}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>
+            Already have an account?
+          </div>
+          <div
+            className="text-netural-800 cursor-pointer hover:underline"
+            onClick={registerModal.onClose}
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   )
 
@@ -66,6 +116,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 }
