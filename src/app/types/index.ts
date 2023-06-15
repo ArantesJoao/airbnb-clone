646c1ba3615c02dbed4503ef9ee1c2c8328e9923
João <arantesjoaov@gmail.com>
2023-06-15 17:53:29 -0300

    sanitized listing entity

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,10 +1,11 @@
-import { User } from "@prisma/client"
+import { Listing, User } from "@prisma/client"
 
-export type SafeUser = Omit<
-  User,
-  "createdAt" | "updatedAt" | "emailVerified"
-> & {
-  createdAt: String
-  updatedAt: String
-  emailVerified: String | null
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string
+}
+
+export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
+  createdAt: string
+  updatedAt: string
+  emailVerified: string | null
 }
