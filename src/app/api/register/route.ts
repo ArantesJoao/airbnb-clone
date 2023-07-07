191ad75717a92fa99cae47bd153d1f3b1a61ca77
João <arantesjoaov@gmail.com>
2023-07-07 16:01:09 -0300

    created listings and favorites pages

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,17 +5,19 @@ import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   const body = await request.json()
-  const { email, name, password } = body
-
+  const { name, email, password } = body
+  console.log(body)
   const hashedPassword = await bcrypt.hash(password, 12)
 
   const user = await prisma.user.create({
     data: {
-      email,
       name,
+      email,
       hashedPassword,
     },
   })
+
+  console.log(user)
 
   return NextResponse.json(user)
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: AuthOptions = {
         })
 
         if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials")
+          throw new Error("This user doesen't exist!")
         }
 
         const isCorrectPassword = await bcrypt.compare(
